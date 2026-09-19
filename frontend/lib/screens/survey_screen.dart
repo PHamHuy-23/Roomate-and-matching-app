541bfcc6f90a6ca37b6882e304a00a65a4e4d92f
Tran Quang Huy <tranquanghuy1029@gmail.com>
2026-09-19 16:57:06 +0700

    fix(survey): import app routes for navigation

--- frontend/lib/screens/survey_screen.dart
+++ frontend/lib/screens/survey_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import '../navigation/app_routes.dart';
 import '../services/api_service.dart';
 
 class SurveyScreen extends StatefulWidget {
